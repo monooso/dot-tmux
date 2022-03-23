f388d0ca2a1d9bc66daed1d59a1bc84b2cda11cb
Stephen Lewis <gpg@stephenlewis.me>
2022-03-23 19:23:31 +0100

    Automatically create the required windows for new tmux sessions

--- bin/tmux-quick-session.sh
+++ bin/tmux-quick-session.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # -----------------------------------------------------------------------------
-# Quickly navigate to any code directory
+# Quickly navigate to any code directory, and create a tmux session
 # -----------------------------------------------------------------------------
 
 # Find all of the "code" directories, and use FZF to fuzzy-find the desired one.
@@ -16,18 +16,21 @@ fi
 selected_name=$(basename "$selected" | tr " -" "_" | tr "[:upper:]" "[:lower:]" | tr -cd "[:alnum:]_")
 tmux_running=$(pgrep tmux)
 
-# $TMUX is set if we're inside a tmux session
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s $selected_name -c $selected
-  exit 0
+# tmux is not currently running, or we don't have the required session.
+# Either way, create the session, but don't attach to it.
+if [[ -z $tmux_running ]] || ! tmux has-session -t $selected_name 2> /dev/null; then
+  # Open three windows: vim, shell, server
+  tmux new-session -s $selected_name -c $selected -n vim -d
+  tmux new-window -t $selected_name -c $selected -n shell
+  tmux new-window -t $selected_name -c $selected -n server
+
+  # Focus the vim window. We can't really launch it, because of nested git worktree directories.
+  tmux select-window -t $selected_name:1
 fi
 
-if ! tmux has-session -t $selected_name 2> /dev/null; then
-  tmux new-session -ds $selected_name -c $selected
-fi
-
-if [[ -z $TMUX ]]; then
-  tmux a -t $selected_name
-else
+# If we're *inside* tmux, switch to the session; otherwise attach to it.
+if [[ -n $TMUX ]]; then
   tmux switch-client -t $selected_name
+else
+  tmux a -t $selected_name
 fi
